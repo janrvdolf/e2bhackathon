6abb1becdd43878c3ef30e617dc022ad5f9a283e
Jan Rudolf <janrvdolf@gmail.com>
2025-03-22 16:42:25 +0100

    Choose the best neural network based on training logs.

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ args_parser.add_argument(
     type=str,
     help="Dir path where are generated neural architectures stored.",
 )
+args_parser.add_argument(
+    "--output_dir_training",
+    default="generated_nn_architectures_training",
+    type=str,
+    help="Dir path where are generated neural architectures stored.",
+)
 args = args_parser.parse_args()
 
 # Neural architecture search
@@ -90,7 +96,45 @@ for i in range(args.number_of_nn_architectures):
                 "mnist_training.py",
                 "--generated_nn_architecture",
                 f"generated_nn_architectures.__output_code_{i}",
-                f">generated_nn_architectures_training/train{i}.txt",
+                f">{args.output_dir_training}/train{i}.txt",
             ]
         )
     )
+
+# Pick the best neural network after training
+
+system_prompt = (
+    "You are a helpful assistant that is also an expert in neural networks." ""
+)
+prompt = f"You are given {args.number_of_nn_architectures} neural network architectures trained on MNIST dataset. Choose the best one and explain why."
+
+train_outputs_prompts = []
+for i in range(args.number_of_nn_architectures):
+    with open(
+        os.path.join(
+            f"{args.output_dir_trainin}/train{i}.txt",
+        ),
+        "r",
+    ) as file:
+        file_content = file.read()
+        train_outputs_prompts.append(
+            f"Train log for neural network {i}:\n{file_content}"
+        )
+
+response = client.messages.create(
+    model="claude-3-5-sonnet-20240620",
+    max_tokens=1024,
+    messages=[
+        {"role": "assistant", "content": system_prompt},
+        {"role": "user", "content": prompt},
+        *[
+            {"role": "user", "content": train_outputs_prompt}
+            for train_outputs_prompt in train_outputs_prompts
+        ],
+    ],
+)
+
+# Extract code from response
+explanation = response.content[0].text
+
+print("The best neural network: ", explanation)
